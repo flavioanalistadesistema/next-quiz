bb0a3dd37d631105ddc4ce1196ebdb46abbc42ea
Flavio Santosm <flavioanalistadesistema@gmail.com>
2022-01-25 15:29:13 -0300

    feat: Iniciando component Question

---
+++ components/Question.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Question.module.css';
+import QuestionModel from "../model/QuestionModel";
+interface QuestionProps {
+    value: QuestionModel
+}
+
+export default function Question(props: QuestionProps) {
+    const question = props.value
+    return (
+        <div className={styles.question}>
+            <h1>Question</h1>
+        </div>
+    )
+}
