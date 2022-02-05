2f98febd95e6f8baf8ecbe52a421f3be43550260
Flavio Santosm <flavioanalistadesistema@gmail.com>
2022-02-05 19:39:20 -0300

    feat: tempo de resposta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import QuestionComponent from "../components/Question.component";
 import QuestionModel from "../model/QuestionModel";
 import AnswerModel from "../model/AnswerModel";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import ButtonComponent from "../components/Button.componet";
 
 
 export default function Home() {
@@ -13,26 +14,36 @@ export default function Home() {
     ])
 
     const [quest, setQuest] = useState(question)
+    const questRef = useRef<QuestionModel>();
+
+    useEffect(() => {
+        questRef.current =  quest
+    }, [quest])
 
     function onAnswer(indice: number) {
-        console.log(question.respondWith(indice), 'respCom')
         setQuest(question.respondWith(indice))
     }
 
     function timeEnd() {
-        if (!question.notAnswer) {
-            setQuest(question.respondWith(-1))
+        if (questRef.current.notAnswer) {
+            setQuest(questRef.current.respondWith(-1))
         }
     }
 
     return (
         <div style={{
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             height: '100vh '
         }}>
-            <QuestionComponent value={quest} onAnswer={onAnswer} timeEnd={timeEnd}/>
+            <QuestionComponent
+                value={quest}
+                onAnswer={onAnswer}
+                timeEnd={timeEnd} 
+                responseTime={5}/>
+            <ButtonComponent text="Próximo" />
         </div>
     )
 }
