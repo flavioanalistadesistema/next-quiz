4d553488db8d717575f7e8f084ca4c66e2edb88a
Flavio Santos <flavio.santos@pravaler.com.br>
2022-03-07 16:45:52 -0300

    feat: component result

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,7 +1,21 @@
+import  styles  from "../styles/Result.module.css";
+import { useRouter } from "next/router"
+
 export default function Result() {
+    const router = useRouter();
+
+    const total = router.query.questions;
+    const correct = router.query.correct;
+    const porcent = Math.round((correct / total) * 100);
+    
+    
     return (
-        <div>
+        <div className={styles.result}>
             <h1>Page Result</h1>
+            <div>Total: {total}</div>
+            <div>Respostas corretas: {correct}</div>
+            <div>Porcentagem de acerto: {`${correct}%`}</div>
+
         </div>
     )
 }
