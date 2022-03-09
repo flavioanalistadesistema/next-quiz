0faec7c92c2011b2715ab432da6f969847938309
Flavio Santos <flavio.santos@pravaler.com.br>
2022-03-09 20:29:20 -0300

    feat: add question

--- pages/api/QuestionData.ts
+++ pages/api/QuestionData.ts
@@ -2,29 +2,71 @@ import QuestionModel from "../../model/QuestionModel";
 import AnswerModel from "../../model/AnswerModel";
 
 const question: QuestionModel[] = [
-    new QuestionModel(1, 'Qual bicho transmite doença de chagas?', [
-        AnswerModel.error('Abelha'),
-        AnswerModel.error('formiga'),
-        AnswerModel.error('mosquito'),
-        AnswerModel.correct('barbeiro')
+    new QuestionModel(1, 'Em qual ano PRAVALER foi fundadda?', [
+        AnswerModel.error('2010'),
+        AnswerModel.error('1990'),
+        AnswerModel.error('1998'),
+        AnswerModel.correct('2001')
     ]),
-    new QuestionModel(2, 'Qual fruto é conhecido no norte e nordeste pelo nome "jerimum"?', [
-        AnswerModel.error('Caju'),
-        AnswerModel.error('Coco'),
-        AnswerModel.error('Maracuja'),
-        AnswerModel.correct('Abobora')
+    new QuestionModel(2, 'Qual o principal objetivo da empresa PRAVALER?', [
+        AnswerModel.error('Um aluno já ta bom'),
+        AnswerModel.error('Dois milhões de alunos'),
+        AnswerModel.error('Dez Alunos'),
+        AnswerModel.correct('um milhão de alunos')
     ]),
-    new QuestionModel(3, 'Qual é o coletivo de caẽs?', [
-        AnswerModel.error('Manada'),
-        AnswerModel.error('Côco'),
-        AnswerModel.error('Chucho'),
-        AnswerModel.correct('Matilha')
+    new QuestionModel(3, 'QUal é o nome do colaborador que é conecido como Maestro?', [
+        AnswerModel.error('Flávio Santos'),
+        AnswerModel.error('Caue Santos'),
+        AnswerModel.error('Fernando Dufour'),
+        AnswerModel.correct('Robson Dias')
     ]),
-    new QuestionModel(4, 'Qual é o triângulo que tem todos os lados diferentes?', [
-        AnswerModel.error('Equilátero'),
-        AnswerModel.error('Isóceles'),
-        AnswerModel.error('Trapézio'),
-        AnswerModel.correct('Escaleno')
+    new QuestionModel(4, 'Qual é o colaborador(a) da nossa equipe, mais velho(a) no PRAVALER?', [
+        AnswerModel.error('Flávio Santos'),
+        AnswerModel.error('Priscilla Spohr'),
+        AnswerModel.error('Thiago Viscardi'),
+        AnswerModel.correct('Thiago Pereira')
+    ]),
+    new QuestionModel(5, 'Qual é o colaborador(a) da nossa equipe, mais novo(a) no PRAVALER?', [
+        AnswerModel.error('João Silveira'),
+        AnswerModel.error('João Luiz'),
+        AnswerModel.error('Cintia Pkubo'),
+        AnswerModel.correct('Priscilla Behnke')
+    ]),
+    new QuestionModel(6, 'Qual colaborador(a) ESPECIALISTA da nossa equipe?', [
+        AnswerModel.error('Caio Giasson'),
+        AnswerModel.error('João Vicente'),
+        AnswerModel.error('Francisco Barroso'),
+        AnswerModel.correct('Rafael Noronha')
+    ]),
+    new QuestionModel(7, 'Qual colaborador(a) TL da nossa equipe?', [
+        AnswerModel.error('Matteo Carminato'),
+        AnswerModel.error('Rodrigo Araujo'),
+        AnswerModel.error('Fernando Dufour'),
+        AnswerModel.correct('Caio Giasson')
+    ]),
+    new QuestionModel(8, 'CEO da empresa?', [
+        AnswerModel.error('Luiz Viela'),
+        AnswerModel.error('TralCI'),
+        AnswerModel.error('Francisco Barroso'),
+        AnswerModel.correct('Carlos Furlan')
+    ]),
+    new QuestionModel(9, 'Qual o melhor presidente para o BRASIL?', [
+        AnswerModel.error('LULA'),
+        AnswerModel.error('BOLSONARO'),
+        AnswerModel.error('TIRIRICA'),
+        AnswerModel.correct('Flávio Santos')
+    ]),
+    new QuestionModel(10, 'Qual funcionário(a) mais bonito(a) da empresa?', [
+        AnswerModel.error('Matteo'),
+        AnswerModel.error('Maestro'),
+        AnswerModel.error('Caio Giasson'),
+        AnswerModel.correct('Flávio Santos')
+    ]),
+    new QuestionModel(11, 'Tech Manager da empresa?', [
+        AnswerModel.error('Rafael Noronha'),
+        AnswerModel.error('Caio Giasson'),
+        AnswerModel.error('João Vicente'),
+        AnswerModel.correct('Francisco Barroso')
     ])
 ]
 
