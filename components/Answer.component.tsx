4f5fffa9b6e11e90241533950e1632eb2fda89a7
Flavio Santos <flavio.santos@pravaler.com.br>
2022-03-09 13:05:58 -0300

    Feat: criando animação nas respostas

--- components/Answer.component.tsx
+++ components/Answer.component.tsx
@@ -11,35 +11,33 @@ interface AnswerComponentProps {
 
 export default function AnswerComponent(props: AnswerComponentProps) {
     const value = props.value
+    const answerRevealed = value.revealed ? styles.answerRevealed : '';    
 
     return (
         <div className={styles.answer}
             onClick={() => props.onAnswer(props.indice)}>
-            <div className={styles.content_answer}>
-                {!value.revealed ? (
-                    <div className={styles.front}>
-                        <div className={styles.letter} style={{backgroundColor: props.backGroundColor}}>
-                            {props.letter}
-                        </div>
-                        <div className={styles.value}>
-                            {value.value}
-                        </div>
+            <div className={`${answerRevealed} ${styles.content_answer}`}>
+                <div className={styles.front}>
+                    <div className={styles.letter} style={{ backgroundColor: props.backGroundColor }}>
+                        {props.letter}
                     </div>
-                ) : (
-                    <div className={styles.verse}>
-                        {value.correct ? (
-                            <div className={styles.correct}>
-                                <div>Sua resposta certa é ....</div>
-                                <div className={styles.value}>{value.value}</div>
-                            </div>
-                        ) : (
-                            <div className={styles.error}>
-                                <div>A resposta selecionada esta errada...</div>
-                                <div className={styles.value}>{value.value}</div>
-                            </div>
-                        )}
+                    <div className={styles.value}>
+                        {value.value}
                     </div>
-                )}
+                </div>
+                <div className={styles.verse}>
+                    {value.correct ? (
+                        <div className={styles.correct}>
+                            <div>Sua resposta certa é ....</div>
+                            <div className={styles.value}>{value.value}</div>
+                        </div>
+                    ) : (
+                        <div className={styles.error}>
+                            <div>A resposta selecionada esta errada...</div>
+                            <div className={styles.value}>{value.value}</div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
